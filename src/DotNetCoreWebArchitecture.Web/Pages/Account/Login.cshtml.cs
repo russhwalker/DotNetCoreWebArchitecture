ea85d57aa6a5f59f41d552e341ba91ee4027bda4
russell walker <russ.h.walker@gmail.com>
2019-05-15 15:34:50 -0400

    adding login page

---
+++ src/DotNetCoreWebArchitecture.Web/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DotNetCoreWebArchitecture.Web.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        private readonly SignInManager<Data.ApplicationUser> signInManager;
+
+        public LoginModel(SignInManager<Data.ApplicationUser> signInManager)
+        {
+            this.signInManager = signInManager;
+        }
+
+        [BindProperty]
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [BindProperty]
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+        public void OnPost()
+        {
+            //var result = signInManager.PasswordSignInAsync(Email, Password, false, false).Result;
+        }
+    }
+}
